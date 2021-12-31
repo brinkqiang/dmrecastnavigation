012e9160ab7ebf3eb86179eb1bfd59b9367fb96f
brinkqiang <brink.qiang@gmail.com>
2021-12-31 16:11:54 +0800

    add

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf build
+mkdir build
+pushd build
+cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+cmake --build . --config relwithdebinfo -- -j$(nproc)
+popd
+
+# echo continue && read -n 1
