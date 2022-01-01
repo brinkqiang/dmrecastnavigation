6388827dc8959dc54450663ccd66993f8ad78b75
brinkqiang <brink.qiang@gmail.com>
2022-01-01 22:30:34 +0800

    add

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo vcpkg install sdl2:x64-linux
 rm -rf build
 mkdir build
 pushd build
-cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+cmake -DCMAKE_BUILD_TYPE=relwithdebinfo -DVCPKG_TARGET_TRIPLET=x64-linux ..
 cmake --build . --config relwithdebinfo -- -j$(nproc)
 
 popd
