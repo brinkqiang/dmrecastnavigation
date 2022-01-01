62d4c78a61bf7a6dc6a90d86b4d56023d557b4e0
brinkqiang <brink.qiang@gmail.com>
2022-01-01 22:30:05 +0800

    add

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo vcpkg install opengl:x64-linux
+echo vcpkg install sdl2:x64-linux
+
 rm -rf build
 mkdir build
 pushd build
 cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
 cmake --build . --config relwithdebinfo -- -j$(nproc)
+
 popd
 
 # echo continue && read -n 1
