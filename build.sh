3421019e5ce847319519cc82329846584cb64001
brinkqiang <brink.qiang@gmail.com>
2022-01-01 22:44:54 +0800

    add

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 echo vcpkg install opengl:x64-linux
 echo vcpkg install sdl2:x64-linux
 
-echo sudo yum install mesa-libGL-devel
-echo sudo yum install freeglut-devel
+echo sudo yum install -y mesa-libGL-devel
+echo sudo yum install -y freeglut-devel
+echo sudo yum install -y SDL2-devel
 
 rm -rf build
 mkdir build
