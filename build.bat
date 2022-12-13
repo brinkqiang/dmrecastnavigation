11627c4170a75e19514b7788381c666cfee855be
brinkqiang <brinkqiang@gmail.com>
2022-12-13 16:56:33 +0800

    ADD

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ rem vcpkg install sdl2:x64-windows-static
 rmdir /S /Q build
 mkdir build
 pushd build
-cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo -DVCPKG_TARGET_TRIPLET=x64-windows-static ..
+cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo -DCMAKE_TOOLCHAIN_FILE=../dmvcpkg/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static ..
 cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
 popd
 
